a46938e42b04f5d86cf4c4b95eb240312ec02a58
Azhar Anwar <azharcodes@gmail.com>
2026-04-09 20:59:13 -0400

    Bump to v0.2.2 (removes sqlite-vec)

--- Formula/muscle-memory.rb
+++ Formula/muscle-memory.rb
@@ -1,7 +1,7 @@
 class MuscleMemory < Formula
   desc "Procedural memory for coding agents"
   homepage "https://github.com/iamazhar/muscle-memory"
-  version "0.2.1"
+  version "0.2.2"
   license "MIT"
 
   on_macos do
