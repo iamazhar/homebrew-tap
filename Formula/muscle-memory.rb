2efe9292f40a0c3047d043fc22488991eba16af4
Azhar Anwar <azharcodes@gmail.com>
2026-04-10 21:07:36 -0400

    Bump muscle-memory to v0.6.0

--- Formula/muscle-memory.rb
+++ Formula/muscle-memory.rb
@@ -1,7 +1,7 @@
 class MuscleMemory < Formula
   desc "Procedural memory for coding agents"
   homepage "https://github.com/iamazhar/muscle-memory"
-  url "https://github.com/iamazhar/muscle-memory/archive/refs/tags/v0.5.0.tar.gz"
+  url "https://github.com/iamazhar/muscle-memory/archive/refs/tags/v0.6.0.tar.gz"
   license "MIT"
 
   depends_on "python@3.12"
